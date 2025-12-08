d9b7e8760b408eb7db70aeb137c737dd775e8153
ComfyCoyote <muzzadamjee@gmail.com>
2025-12-07 19:14:09 -0800

    Updated next config image remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,27 @@
 import type { NextConfig } from "next";
 
+ //[new URL('https://d4ixhj8jfp690.cloudfront.net/**'), new URL('https://items-images-production.s3.us-west-2.amazonaws.com/**')], // allow external image domains
+
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [new URL('https://d4ixhj8jfp690.cloudfront.net/**'), new URL('https://items-images-production.s3.us-west-2.amazonaws.com/**')], // allow external image domains
+    qualities: [60, 75],
+    remotePatterns:  [
+     {
+        protocol: 'https',
+        hostname: 'd4ixhj8jfp690.cloudfront.net',
+        pathname: "/**"
+    },
+    {
+      protocol: 'https',
+      hostname: 'items-images-production.s3.us-west-2.amazonaws.com',
+      pathname: "/**"
+    }
+  ]
+   
   },
 };
 
 export default nextConfig;
+
+
+ 
